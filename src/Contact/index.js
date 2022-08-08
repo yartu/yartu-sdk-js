13ebd86a816d414b04f8c58ea0214830f8764765
Ahmet <ahmet@yartu.io>
2022-08-08 15:18:50 +0300

    feat: add list contact

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,4 +1,4 @@
-import { ListAddressBookRequest } from './service-pb.cjs';
+import { ListAddressBookRequest, ListContactRequest } from './service-pb.cjs';
 import { YContactClient } from './service-grpc-web-pb.cjs';
 
 export default (config) =>
@@ -20,6 +20,44 @@ export default (config) =>
         const request = new ListAddressBookRequest();
         // request.setQuery()
         this.client.listAddressBook(
+          request,
+          this.metadata,
+          (error, response) => {
+            if (error) {
+              reject({
+                code: -1,
+                message: error.message
+              });
+            } else {
+              const code = response.getCode();
+
+              if (code == 0) {
+                const dataList = response
+                  .getDataList()
+                  .map((data) => data.toObject());
+                resolve({
+                  data: dataList,
+                  pagination: response.getPagination().toObject()
+                });
+              } else {
+                reject({
+                  code: code,
+                  message: response.getMessage()
+                });
+              }
+            }
+          }
+        );
+      });
+    };
+
+    listContact = (addressBookId, query = {}) => {
+      return new Promise((resolve, reject) => {
+        const request = new ListContactRequest();
+        // request.setQuery([]);
+        request.setAddressBookId(addressBookId);
+
+        this.client.listContact(
           request,
           this.metadata,
           (error, response) => {
