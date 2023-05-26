dfbb9a79190da7d01fe2b1997c82b142746dceb1
Ahmet Küçük <akucuk@yartu.io>
2023-05-26 18:50:34 +0300

    feat: add external apps

---
+++ src/ExternalApp/index.js
@@ -0,0 +1,48 @@
+import { YAppClient } from './service-grpc-web-pb.cjs';
+import { GetAppTokenRequest } from './service-pb.cjs';
+import { handleError } from '../utils/helper';
+
+export default (config) =>
+  class ExternalApp {
+    endpoint = 'http://localhost:5001';
+    client = undefined;
+    yartuSdk = undefined;
+    shareUuid = undefined;
+
+    constructor(config) {
+      this.endpoint = config.endpoint;
+      this.client = new YAppClient(this.endpoint, '', '');
+      const yartu_token = window.localStorage.getItem('yartu-token');
+      this.metadata = { Authentication: yartu_token };
+    }
+
+    getAppToken = (appUuid) => {
+      return new Promise((resolve, reject) => {
+        const request = new GetAppTokenRequest();
+
+        request.setAppUuid(appUuid);
+
+        this.client.getAppToken(request, this.metadata, (error, response) => {
+          if (error) {
+            handleError(error, reject);
+          } else {
+            const code = response.getCode();
+
+            if (code == 0) {
+              resolve({
+                code,
+                token: response.getToken(),
+                url: response.getUrl(),
+                message: response.getMessage()
+              });
+            } else {
+              reject({
+                code: code,
+                message: response.getMessage()
+              });
+            }
+          }
+        });
+      });
+    };
+  };
