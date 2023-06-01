2fffbb09eba2df5b795389f5ab8960c198aeee9e
ramazan <ramazan@yartu.io>
2023-06-01 16:08:16 +0300

    fix: customPermissionMap toObject cannot converts map arr to obj situation handled.

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -10,4 +10,23 @@ const handleError = (error, reject) => {
   });
 };
 
-export { handleError };
+// jspb.Map.prototype.loadFromArray_ = function() {
+//   for (var i = 0; i < this.arr_.length; i++) {
+//     var record = this.arr_[i];
+//     var key = record[0];
+//     var value = record[1];
+//     this.map_[key.toString()] = new jspb.Map.Entry_(key, value);
+//   }
+//   this.arrClean = true;
+// };
+const loadFromArray = (unmappedArrArr) => {
+  const resp = {};
+  for (let i = 0; i < unmappedArrArr.length; i++) {
+    const record = unmappedArrArr[i];
+    const key = record[0];
+    resp[key] = record[1];
+  }
+  return resp;
+};
+
+export { handleError, loadFromArray };
