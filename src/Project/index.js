915f6e4ab58001e929fc13b396f72d14e9b06d91
ramazan <ramazan@yartu.io>
2023-06-20 10:54:29 +0300

    feat: data resolved for addInteractionToCard service.

--- src/Project/index.js
+++ src/Project/index.js
@@ -1544,8 +1544,10 @@ export default (config) =>
             } else {
               const code = response.getCode();
               if (code == 0) {
+                const data = response.getData().toObject();
                 resolve({
                   code,
+                  data,
                 })
               } else {
                 reject({
