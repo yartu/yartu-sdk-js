de7cc49d6238d97c6da735ba76ae2cdc14584791
Mücahit Büyükyılmaz <mucahit@yartu.io>
2026-06-24 23:05:11 +0300

    feat: update sanitizer

--- src/utils/xss_filter.js
+++ src/utils/xss_filter.js
@@ -1,203 +1,121 @@
 // eslint-disable-next-line unicorn/filename-case
-// TODO :: all actions should be dynamic. from main @akucuk
-import sanitizeHtml from 'sanitize-html';
+import DOMPurify from 'dompurify';
 
-export const xssOptions = (yartuAttach = {}) => {
-  const imgInlineList = [];
-  const options = {
-    allowedTags: [
-      ...sanitizeHtml.defaults.allowedTags,
-      'img',
-      'h1',
-      'h2',
-      'span',
-      'em',
-      'b',
-      'blockquote',
-      'strong',
-      'table',
-      'tbody',
-      'center',
-      'font',
-      'title',
-      'link',
-      'head',
-      'html'
-    ],
-    allowedAttributes: {
-      '*': [
-        'height',
-        'width',
-        'style',
-        'align',
-        'alink',
-        'alt',
-        'bgcolor',
-        'border',
-        'cellpadding',
-        'cellspacing',
-        'class',
-        'color',
-        'cols',
-        'colspan',
-        'coords',
-        'dir',
-        'face',
-        'hspace',
-        'ismap',
-        'lang',
-        'marginheight',
-        'marginwidth',
-        'multiple',
-        'nohref',
-        'noresize',
-        'noshade',
-        'nowrap',
-        'ref',
-        'rel',
-        'rev',
-        'rows',
-        'rowspan',
-        'scrolling',
-        'shape',
-        'span',
-        'summary',
-        'tabindex',
-        'title',
-        'usemap',
-        'valign',
-        'value',
-        'vlink',
-        'vspace',
-        'data-yartu-href',
-        'data-yartu-src'
-      ],
-      a: ['href', 'target', 'data-yartu'],
-      img: ['src', 'border', 'width', 'height', 'id', 'style', 'yartu-name'],
-      td: [
-        'colspan',
-        'headers',
-        'rowspan',
-        'abbr',
-        'char',
-        'charoff',
-        'scope',
-        'align',
-        'valign',
-        'bgcolor',
-        'width',
-        'height'
-      ],
-      tr: ['align', 'valign', 'bgcolor', 'width', 'height'],
-      table: [
-        'background',
-        'bgcolor',
-        'frame',
-        'rules',
-        'summary',
-        'border',
-        'cellpadding',
-        'cellspacing',
-        'header-table',
-        'align',
-        'width',
-        'style'
-      ],
-      link: ['href', 'rel'],
-      div: ['align', 'data-signature', 'data-type'],
-      p: ['data-signature', 'data-type', 'data-yartu']
-    },
-    allowedSchemes: ['http', 'https', 'mailto', 'tel'],
-    allowedSchemesByTag: {
-      img: ['data', 'src']
-    },
-    transformTags: {
-      a: (tagName, attribs) => {
-        const attribute = { ...attribs };
-        let href = `${attribute.href}`;
-        const { host } = window.location;
+const purify = DOMPurify(window);
 
-        attribute['data-yartu-href'] = href;
+const ADD_TAGS = ['font', 'center'];
+const ADD_ATTR = [
+  'target',
+  'align',
+  'valign',
+  'bgcolor',
+  'background',
+  'border',
+  'cellpadding',
+  'cellspacing',
+  'color',
+  'cols',
+  'colspan',
+  'rows',
+  'rowspan',
+  'face',
+  'nowrap',
+  'marginheight',
+  'marginwidth',
+  'hspace',
+  'vspace',
+  'alink',
+  'vlink',
+  'id',
+  'style',
+  'class',
+  'yartu-name',
+  'data-yartu',
+  'data-yartu-href',
+  'data-yartu-src',
+  'data-signature',
+  'data-type',
+];
 
-        if (
-          href &&
-          !href.includes('/public/url/') &&
-          !href.includes('mailto:') &&
-          !href.includes('tel:') &&
-          !href.includes('fax:')
-        ) {
-          try {
-            const aHost = new URL(href).host;
-            if (host !== aHost) {
-              const base64Url = Buffer.from(href, 'utf8').toString('base64');
-              href = `/#/public/url/?u=${base64Url}`;
-            }
-          } catch {
-            // pass
-            attribute['data-yartu'] = 'wrong-url';
-          }
-        }
+let ctx = { imgInlineList: [], uuid: undefined };
 
-        if (href) {
-          attribute.href = href;
-        }
+purify.addHook('afterSanitizeAttributes', (node) => {
+  const tag = node.tagName;
 
-        attribute.target = '_blank';
+  if (tag === 'A') {
+    const original = node.getAttribute('href');
+    if (!original) {
+      return;
+    }
+    node.setAttribute('data-yartu-href', original);
 
-        return {
-          tagName: 'a',
-          attribs: attribute
-        };
-      },
-      img: (tagName, attribs) => {
-        let source = attribs.src;
-        if (source) {
-          const yartuName = attribs['yartu-name'];
-          attribs['data-yartu-src'] = source;
+    let href = original;
+    const skipProtocols = new Set(['mailto:', 'tel:', 'fax:']);
+    try {
+      const parsed = new URL(href); // relative/invalid URL => catch
+      const isSameHost = parsed.host === window.location.host;
+      const isInternalPublicUrl =
+        isSameHost && parsed.pathname.startsWith('/public/url/');
+      if (
+        !skipProtocols.has(parsed.protocol) &&
+        !isInternalPublicUrl &&
+        !isSameHost
+      ) {
+        const base64Url = Buffer.from(href, 'utf8').toString('base64');
+        href = `/#/public/url/?u=${base64Url}`;
+      }
+    } catch {
+      node.setAttribute('data-yartu', 'wrong-url');
+    }
 
-          if (source.includes('cid:')) {
-            attribs['yartu-name'] = 'yartu-attach-file';
-            const cid = source.replace('cid:', '');
-            source = `/file/attachment/${yartuAttach.uuid}?cid=${cid}`;
-            imgInlineList.push(cid);
-          } else if (!yartuName) {
-            attribs['yartu-name'] = 'yartu-image';
-            if (source.includes('//')) {
-              const base64 = Buffer.from(attribs.src, 'utf8').toString(
-                'base64'
-              );
-              source = `/img/not-allowed.png?base=/${base64}`;
-            }
-          }
-        }
+    node.setAttribute('href', href);
+    node.setAttribute('target', '_blank');
+    node.setAttribute('rel', 'noopener noreferrer');
+    return;
+  }
 
-        if (source) {
-          attribs.src = source;
-        }
+  if (tag === 'IMG') {
+    const original = node.getAttribute('src');
+    if (!original) {
+      return;
+    }
+    const yartuName = node.getAttribute('yartu-name');
+    node.setAttribute('data-yartu-src', original);
 
-        return {
-          tagName: 'img',
-          attribs
-        };
+    let source = original;
+    if (original.includes('cid:')) {
+      node.setAttribute('yartu-name', 'yartu-attach-file');
+      const cid = original.replace('cid:', '');
+      source = `/file/attachment/${ctx.uuid}?cid=${cid}`;
+      ctx.imgInlineList.push(cid);
+    } else if (!yartuName) {
+      node.setAttribute('yartu-name', 'yartu-image');
+      if (/^https?:\/\//i.test(original) || original.startsWith('//')) {
+        const base64 = Buffer.from(original, 'utf8').toString('base64');
+        source = `/img/not-allowed.png?base=/${base64}`;
       }
     }
-  };
-  return {
-    options,
-    imgInlineList
-  };
-};
+
+    node.setAttribute('src', source);
+  }
+});
+
+export const xssOptions = () => ({
+  options: { ADD_TAGS, ADD_ATTR },
+  imgInlineList: [],
+});
 
 export const sanitize = (uglyHtml, customOptions = {}, yartuAttach = {}) => {
-  const { options, imgInlineList } = xssOptions(yartuAttach);
-  const sanitizeResponse = sanitizeHtml(uglyHtml, {
-    ...options,
-    ...customOptions
+  ctx = { imgInlineList: [], uuid: yartuAttach.uuid };
+  const sanitizeResponse = purify.sanitize(uglyHtml || '', {
+    ADD_TAGS,
+    ADD_ATTR,
+    ...customOptions,
   });
 
   return {
     sanitize: sanitizeResponse,
-    imgInlineList
+    imgInlineList: ctx.imgInlineList,
   };
 };
 
